05fb305a5d8dcb6866618a9b94568d735740137f
Winkerlied <83118447+Winkerlied@users.noreply.github.com>
2021-10-09 00:02:06 +0200

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,3 +1,3 @@
 # 'source alias.sh' for defined aliases to take an effect
-alias b='sh build.b'
+alias b='sh build.sh'
 alias r='make; if [ $? -eq 0 ]; then clear; echo; echo; ./main; fi; echo; echo;'
