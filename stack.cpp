7d6b154c651f35c610d332d19c8100b55500dd99
Winkerlied <83118447+Winkerlied@users.noreply.github.com>
2021-10-08 22:54:20 +0200

    Create stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,24 @@
+typedef struct stack tstack
+{
+  
+};
+tstack *init(void)
+{
+
+}
+void close(tstack *s)
+{
+
+}
+int isempty(tstack *s)
+{
+
+}
+int push(tstack *s, int el)
+{
+
+}
+int pop(tstack *s)
+{
+
+}
