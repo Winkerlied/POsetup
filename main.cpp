2b0059857976bfdcd521614a6722e82c6e83bd31
Winkerlied <83118447+Winkerlied@users.noreply.github.com>
2021-10-09 10:47:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include "stack.h"
-
+#include <stdio.h>
 int main()
 {
   // main code
+  tstack* one = init();
+  tstack* two = init();
+  if(one!=two)
+    printf("[0]\tStacks are different\m");
+  else
+    printf("[0]\tone and two point at the same adress. Shouldn't be that way.\n");
+  { // operations on empty stack
+      pop(one);
+      close(one);
+      close(one);
+  }
+  { // push on uninitialized/closed stack
+      push(one,132);
+      tstack* three;
+      push(three,5256);
+  }
+  {
+    if( push(two,3333) == 3333)
+      printf("[1]\tpush works properly\n");
+    else
+      printf("[1]\tpush may not work properly\n");
+    if(pop(two)==3333)
+      printf("[1]\tpop works properly\n");
+    else
+      printf("[1]\tpop may not work properly\n");
+    pop(two);
+    
+  }
 }
